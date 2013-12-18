0ef1c2631dfe1a37f5ab0d18d7d6840212f56279
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-12-18 17:05:41 -0500

    One emitter, same listener in a global namespace.

---
+++ examples/example6.js
@@ -0,0 +1,19 @@
+// GLOBAL NAMESPACE - ONE EMITTER, SAME LISTENER
+var eventerface = require('../eventerface');
+
+// Create a global namespace by providing the desired name
+// and a callback function to receive the evented interface object
+eventerface.create('/app', function (app) {
+    // Start emitting and listening to events using the provided 'app' emitter
+    app.on('hey', function () {
+        console.log('Hey!');
+    });
+    app.emit('hey');
+});
+
+/* OUTPUT:
+    [Nothing. Emitters do not listen to their own events]
+*/
+
+
+
