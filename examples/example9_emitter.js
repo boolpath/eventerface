af383489f3085c3988bdf6ffef6d63c320d0a5e1
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-12-18 20:45:03 -0500

    One emitter, many listeners in different files using a global namespace.

---
+++ examples/example9_emitter.js
@@ -0,0 +1,11 @@
+// GLOBAL NAMESPACE - MANY EMITTERS (ONE IN THIS FILE), ONE LISTENER (IN ANOTHER FILE)
+var eventerface = require('../eventerface');
+
+// Create a global namespace by providing the desired name
+// and a callback function to receive the evented interface object
+eventerface.create('/app', function (app) {
+    // Emit an event to the 'app' global namespace for other members to handle
+    setInterval(function () {
+        app.emit('helloWorld');
+    }, 1000);
+});
---
+++ examples/example9_listener1.js
@@ -0,0 +1,18 @@
+// GLOBAL NAMESPACE - ONE EMITTER (IN ANOTHER FILE), MANY LISTENER (ONE IN THIS FILE)
+var eventerface = require('../eventerface');
+
+// Find the existing 'app' global namespace
+// (created in example9_emitter.js)
+eventerface.find('/app', function (app) {
+    // Handle events emitted by other members of the 'app' global namespace
+    app.on('helloWorld', function () {
+        console.log('Hello!');
+    });
+});
+
+/* OUTPUT:
+    'Hello!'
+    'Hello!'
+    'Hello!'
+    ...
+*/
