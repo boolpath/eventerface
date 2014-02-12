a7c3c9873fd27ef106ff3f2f0ce417db0ac02434
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-12 11:18:34 -0500

    Added documentation and inline comments.

--- eventerface.js
+++ eventerface.js
@@ -1,5 +1,3 @@
-/* NODE MODULES */
-
 /** LOCAL OBJECT
  * @property {object} factory - A module for creating objects
  * @property {object} map - A module for adding an retrieving event bindings
@@ -10,7 +8,7 @@ var EVENTERFACE = {
 }; 
 
 /** MODULE INTERFACE 
- * @method {function} create - Creates a new evented interface, either as a routing channel or a RESTful API
+ * @method {function} create - Creates a new evented interface, either as a routing channel or a RESTlike API
  * @method {function} find - Finds an existing evented interface, either locally or remotely
  * @method {function} bind - Binds the events of an emitter object to the parties of interest
  */
@@ -21,17 +19,20 @@ module.exports = {
 
 /*----------------------------------------------------------------------------*/
 
-/** Creates a new evented interface, either as a routing channel or a RESTful API
+/** Creates a new evented interface, either as a routing channel or a RESTlike API
  * @param {object} options - Describes the features of the interface
- * @returns
+ * @param {function} callback - A function to call when the eventerface is created
+ * @returns {object} newEventerface - The eventerface object if a callback was no provided
  */
 function create(options, callback) {
-    var newEventerface, 
-        mapping,
-        mappingOptions;
+    var newEventerface, // Holds the created eventerface
+        mappingOptions, // Describes the type of mapping according to the options parameter
+        mapping;        // The event mapping object created according to mappingOptions
 
+    // Inspect options
     switch (typeof options) {
     case 'undefined':
+        // #create(): local namespace
         mappingOptions = { 
             scope: 'local', 
             type: 'namespace'
@@ -39,6 +40,7 @@ function create(options, callback) {
         break;
     case 'string':
         if (options.indexOf('/') === -1) {
+            // #create(app): local namespace 'app'
             mappingOptions = { 
                 name: options,
                 scope: 'local', 
@@ -46,6 +48,7 @@ function create(options, callback) {
             };
         } else if (options.indexOf('/') === 0) {
             if (options.lastIndexOf('/') === 0) {
+            // #create(/app): global namespace 'app'
                 mappingOptions = {
                     name: options.split('/')[1],
                     scope: 'global',
@@ -61,13 +64,14 @@ function create(options, callback) {
         break;
     }
 
-    console.log('Creating ' + mappingOptions.scope 
-                + ' ' + mappingOptions.type 
-                + ' "' + (mappingOptions.name || '') + '"');
+    console.log('Creating ' + mappingOptions.scope, mappingOptions.type, mappingOptions.name || '');
 
-    mapping = EVENTERFACE.maps.create(mappingOptions);
-    newEventerface = EVENTERFACE.factory.createInterface(mapping);
+    // Create the event mapping object accoding to the specified options
+        mapping = EVENTERFACE.maps.create(mappingOptions);
+    // Create the eventerface using the mapping object
+        newEventerface = EVENTERFACE.factory.createInterface(mapping);
 
+    // Return the created eventerface through a callback or by reference
     if (mappingOptions.scope === 'global' && typeof callback === 'function') {
         EVENTERFACE.maps.find(mappingOptions, function (eventerface) {
             callback(eventerface);
@@ -78,12 +82,11 @@ function create(options, callback) {
 }
 
 /** Finds an existing evented interface, either locally or remotely
- * @param {object} options - Information needed to find the interface
- * @returns
+ * @param {object} options - Information needed to find the eventerface
+ * @param {function} onFound - A callback to invoke when the eventerface is found
  */
 function find(options, onFound) {
-    var theEventerface,
-        map;
+    var map;
 
     switch (typeof options) {
     case 'undefined':
@@ -93,6 +96,7 @@ function find(options, onFound) {
         if (options.indexOf('/') === -1) {
             
         } else if (options.indexOf('/') === 0) {
+            // #find(/app): global namespace 'app'
             if (options.lastIndexOf('/') === 0) {
                 map = {
                     name: options.split('/')[1],
