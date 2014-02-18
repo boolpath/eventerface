19241b7456c4b0ca1e55f580587f8dcaf8481930
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-18 14:12:02 -0500

    Fixed a call stack bug on local namespaces and removed the events array.

--- maps/namespace/local.js
+++ maps/namespace/local.js
@@ -1,26 +1,19 @@
 /** Creates a simple local mapper that routes every event to its listeners
- * @returns {object} map - The local map created
+ * @returns {object} map - The created event mapping context
  */
 module.exports = function () {
     var map = {
-        events: {},     // A set of the emitted events
         trees: {}       // A set of arrays of listeners of events
     };
 
     // Map emitted events to all listeners by emitting the event on each of them
     map.emit = function (eventName, message, emitter) {
-        // Register the event if this is the first time it is emitted
-        var eventEmitter = map.events[eventName];
-        if (!eventEmitter) {
-            map.events[eventName] = eventEmitter = emitter;
-        }
-
         // If there are any registered listeners to this particular event:
         var eventListeners = map.trees[eventName];
         if (eventListeners) {
             // Send the event to each listener
             eventListeners.forEach(function (listener) {
-                if (typeof listener === 'object' && eventEmitter !== listener &&
+                if (typeof listener === 'object' && emitter !== listener &&
                     typeof listener.emit === 'function') {
                     listener.ownEmit(eventName, message);
                 }
